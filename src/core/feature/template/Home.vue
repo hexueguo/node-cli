2422ae0c9b68ece0ee9b88a8d223c7afe4b9673f
Lecea <42921767+hexueguo@users.noreply.github.com>
2021-07-14 16:57:16 +0800

    Create Home.vue

---
+++ src/core/feature/template/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
